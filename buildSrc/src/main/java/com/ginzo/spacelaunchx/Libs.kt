c20bfc935e09bf6080f515e4c8f59d4c333adaaf
Gerard <gserrano@gyngi.com>
2019-10-11 17:56:13 +0200

    Configure gradle

--- buildSrc/src/main/java/com/ginzo/spacelaunchx/Libs.kt
+++ buildSrc/src/main/java/com/ginzo/spacelaunchx/Libs.kt
@@ -1,3 +1,31 @@
 package com.ginzo.spacelaunchx
 
-object Libs {}
+object Libs {
+  object kotlin {
+    val kotlinVersion = "1.3.50"
+
+    val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion"
+
+    object gradle {
+      val core = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
+    }
+  }
+
+  val coreKtx = "androidx.core:core-ktx:1.0.2"
+
+  val constrainLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+
+  object androidx {
+    val appCompat = "androidx.appcompat:appcompat:1.0.2"
+  }
+
+  object google {
+    val material = "com.google.android.material:material:1.0.0"
+  }
+}
+
+object Conf {
+  const val compileSdkVersion = 28
+  const val targetSdkVersion = 28
+  const val minSdkVersion = 21
+}
